432248fc979d0251c29be2c48587b6baa29d18ce
Cliffy2000 <cliffyyc@outlook.com>
2024-10-01 13:16:26 -0400

    assignment 2 setup

---
+++ Assignment2/src/brewery/Brewery.java
@@ -0,0 +1,7 @@
+package brewery;
+
+public class Brewery {
+    public static void run() {
+        System.out.println("Brewery starting. ");
+    }
+}
---
+++ Assignment2/src/item/Item.java
@@ -0,0 +1,27 @@
+package item;
+
+public class Item {
+    private String name;
+    private int count;
+
+    public Item(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
