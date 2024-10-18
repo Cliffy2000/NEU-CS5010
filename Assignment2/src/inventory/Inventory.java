ed9c72c64cfe31c964e678727c07cf0fe5ecf4ab
Cliff <cliffyyc@outlook.com>
2024-10-18 17:21:06 -0400

    inventory and library

--- Assignment2/src/inventory/Inventory.java
+++ Assignment2/src/inventory/Inventory.java
@@ -1,4 +1,92 @@
 package inventory;
 
+import item.*;
+
 public class Inventory {
+    private Item[] drinkInventory;
+    private int drinkTypeCount;
+    private Item[] ingredientInventory;
+    private int ingredientTypeCount;
+
+    public Inventory() {
+        this.drinkInventory = new Item[4];
+        this.drinkTypeCount = 0;
+        this.ingredientInventory = new Item[4];
+        this.ingredientTypeCount = 0;
+    }
+
+    public int getDrinkCount(String drinkName) {
+        for (int i = 0; i < this.drinkTypeCount; i++) {
+            if (this.drinkInventory[i].getName().equals(drinkName)) {
+                return this.drinkInventory[i].getCount();
+            }
+        }
+        // The desired drink is not included in the inventory
+        addDrinkType(drinkName, 0);
+        return 0;
+    }
+
+    public void addDrinkType(String drinkName, int drinkCount) {
+        if (this.drinkTypeCount >= this.drinkInventory.length) {
+            Item[] newList = new Item[this.drinkInventory.length * 2];
+            // Manual array copy to avoid imports
+            for (int i = 0; i < this.drinkInventory.length; i++) {
+                newList[i] = this.drinkInventory[i];
+            }
+            this.drinkInventory = newList;
+        }
+
+        this.drinkInventory[this.drinkTypeCount] = new Item(drinkName, drinkCount);
+        this.drinkTypeCount += 1;
+    }
+
+    // This function only changes the value, any checks should be made before the function is used
+    public void adjustDrinkCount(String drinkName, int delta) {
+        int index = 0;
+        // Find the drink in the inventory
+        while (true) {
+            if (this.drinkInventory[index].getName().equals(drinkName)) {
+                break;
+            }
+            index += 1;
+        }
+        this.drinkInventory[index].setCount(this.drinkInventory[index].getCount() + delta);
+    }
+
+    public int getIngredientCount(String ingredientName) {
+        for (int i = 0; i < this.ingredientTypeCount; i++) {
+            if (this.ingredientInventory[i].getName().equals(ingredientName)) {
+                return this.ingredientInventory[i].getCount();
+            }
+        }
+        // The desired ingredient is not included in the inventory
+        addIngredientType(ingredientName, 0);
+        return 0;
+    }
+
+    public void addIngredientType(String ingredientName, int ingredientCount) {
+        if (this.ingredientTypeCount >= this.ingredientInventory.length) {
+            Item[] newList = new Item[this.ingredientInventory.length * 2];
+            // Manual array copy to avoid imports
+            for (int i = 0; i < this.ingredientInventory.length; i++) {
+                newList[i] = this.ingredientInventory[i];
+            }
+            this.ingredientInventory = newList;
+        }
+
+        this.ingredientInventory[this.ingredientTypeCount] = new Item(ingredientName, ingredientCount);
+        this.ingredientTypeCount += 1;
+    }
+
+    public void adjustIngredientCount(String ingredientName, int delta) {
+        int index = 0;
+        // Find the ingredient in the inventory
+        while (true) {
+            if (this.ingredientInventory[index].getName().equals(ingredientName)) {
+                break;
+            }
+            index += 1;
+        }
+        this.ingredientInventory[index].setCount(this.ingredientInventory[index].getCount() + delta);
+    }
 }
--- Assignment2/src/library/Library.java
+++ Assignment2/src/library/Library.java
@@ -4,8 +4,39 @@ import item.*;
 
 public class Library {
     private Recipe[] recipeList;
+    private int recipeCount;
+
+    public Library() {
+        this.recipeList = new Recipe[4];
+        this.recipeCount = 0;
+    }
 
     public void addRecipe(Recipe recipe) {
+        // Creates a larger array to store recipes if the current one is full
+        if (this.recipeCount >= this.recipeList.length) {
+            Recipe[] newList = new Recipe[this.recipeList.length * 2];
+            // Manual array copy to avoid imports
+            for (int i = 0; i < this.recipeList.length; i++) {
+                newList[i] = this.recipeList[i];
+            }
+            this.recipeList = newList;
+        }
 
+        // Array indexing starts at 0, count starts at 1
+        recipeList[recipeCount] = recipe;
+        recipeCount += 1;
+        System.out.printf("The recipe for %s has been added to the library.\n", recipe.getName());
+    }
+
+    public Recipe queryRecipe(String recipeName) {
+        for (int i = 0; i < this.recipeCount; i++) {
+            if (this.recipeList[i].getName().equals(recipeName)) {
+                return this.recipeList[i];
+            }
+        }
+
+        // Failed to find recipe with this name
+        System.out.printf("Could not find the recipe for %s.\n", recipeName);
+        return null;
     }
 }
